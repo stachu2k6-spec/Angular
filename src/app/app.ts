499655890c05070d69dece0932fb1dca79109546
Stanislaw Jankowski <stachu2k6@gmail.com>
2025-10-08 17:18:17 +0200

    signature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Sign} from "node:crypto";
+
+export type Signature = 'C' | '/' | '*' | '-' | '+' | '=' | '%' | '.' | '+/-' | 'sqrtx' | 'x^2' | '1/x' | 'MC' | 'MR' | 'M+' | 'M-' | 'Mv' | 'MS' | 'CE' | 'backspace';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('calculator');
+    result: String = '0';
+
+    clickCalculate(value: Signature) {
+        console.log("buttonClick", value);
+    }
+
+    clickNumber(value: number) {
+        console.log("buttonClick", value);
+        if(this.result == '0'){
+            this.result = value + '';
+        } else {
+            this.result = this.result + '' + value;
+        }
+    }
+
+
 }
